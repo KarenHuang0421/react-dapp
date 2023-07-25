0ff156cd93ac446acc82bc3d5067fea31bad9b02
KarenHuang0421 <a0988423161@gmail.com>
2023-07-25 23:48:19 +0800

    init

---
+++ src/utils/config.js
@@ -0,0 +1,29 @@
+export const CONTRACT_ADDRESS = '0xd9145CCE52D386f254917e481eB44e9943F39138';
+export const CONTRACT_ABI = [
+  {
+    inputs: [],
+    name: "retrieve",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "num",
+        type: "uint256",
+      },
+    ],
+    name: "store",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
